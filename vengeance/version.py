bdfdbc9dd9b44274549d3983c7891083d88d2390
michael-ross-ven <michael.ross.uncc@gmail.com>
2019-03-21 19:27:37 -0400

    removed __pycache__

--- vengeance/version.py
+++ vengeance/version.py
@@ -1,9 +1,4 @@
 
-__version__ = '1.0.1'
-__release__ = '$release 1'
-
-dependencies = ('comtypes',
-                'pypiwin32',
-                'line_profiler',
-                'pandas',
-                'pyodbc')
+# generated from setup.py
+__version__  = '1.0.1'
+dependencies = ('comtypes', 'pypiwin32', 'line_profiler', 'pandas', 'pyodbc')
