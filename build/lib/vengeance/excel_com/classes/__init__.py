d1168bdda5d890ad995e929e0dcee7a2e845d4b7
michael-ross-ven <michael.ross.uncc@gmail.com>
2020-12-24 23:17:04 -0500

    version: 1.1.5, $release 43

--- build/lib/vengeance/excel_com/classes/__init__.py
+++
@@ -1,4 +0,0 @@
-
-from .excel_levity_cls import excel_levity_cls
-
-__all__ = ['excel_levity_cls']
--- build/lib/vengeance/version.py
+++
@@ -1,7 +0,0 @@
-
-""" https://github.com/michael-ross-ven/vengeance/blob/master/releases.log """
-__version__ = '1.1.5'
-__release__ = '$release 43'
-
-__all__ = ['__version__',
-           '__release__']
