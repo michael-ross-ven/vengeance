0c494b121eca466531e87b135d1890f2fe4d5dc1
michael-ross-ven <michael.ross.uncc@gmail.com>
2021-02-26 14:28:37 -0500

    version: 1.1.12, $release 49

--- vengeance/version.py
+++ vengeance/version.py
@@ -1,7 +1,7 @@
 
 """ https://github.com/michael-ross-ven/vengeance/blob/master/releases.log """
-__version__ = '1.1.11'
-__release__ = '$release 48'
+__version__ = '1.1.12'
+__release__ = '$release 49'
 
 __all__ = ['__version__',
            '__release__']
