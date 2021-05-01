910202abd6eda4517682274676ab538507f07598
michael-ross-ven <michael.ross.uncc@gmail.com>
2021-05-01 18:27:14 -0400

    removed build folders

--- build/lib/vengeance/version.py
+++
@@ -1,7 +0,0 @@
-
-""" https://github.com/michael-ross-ven/vengeance/blob/master/releases.log """
-__version__ = '1.1.15'
-__release__ = '$release 52'
-
-__all__ = ['__version__',
-           '__release__']
